f99dab4bd6ec22aa353a64abca8b8aace46f2604
Matt Barnes <mbarnes2510@googlemail.com>
2022-02-04 15:15:43 +0000

    split up taxonomy plotting for ease

---
+++ taxonomy_analysis.py
@@ -0,0 +1,108 @@
+from numpy.lib.type_check import nan_to_num
+
+from sklearn.metrics import r2_score
+from sklearn.cluster import AgglomerativeClustering
+
+import scipy.stats as stats
+import numpy as np
+
+
+def build_taxonomy_data_dict(plot_data_dict, add_net_stats=True):
+    taxonomy_data_dict = {'mobility': {}, 'assortativity': {}, 'philanthropy': {},
+                          'community': {}, 'delta_assortativity': {}, 'neighbourhood_mobility': {}}
+    if add_net_stats:
+        taxonomy_data_dict['equality'] = {}
+        # , 'nodes': {}, 'edges': {}}
+
+    for data_f_name, plot_data in plot_data_dict.items():
+
+        taxonomy_data = plot_data['taxonomy_data']
+        if add_net_stats:
+            stats_data = plot_data['stats_data']
+        t = plot_data['t']
+        dt = plot_data['dt']
+        data_type = plot_data['data_type']
+        struc_type = plot_data['struc_type']
+
+        data_label = f"{data_f_name}: t={t} dt={dt} #{data_type}${struc_type}"
+
+        individual = taxonomy_data['individual']
+        delta_individual = taxonomy_data['delta_individual']
+        neighbourhood = taxonomy_data['neighbourhood']
+        delta_neighbourhood = taxonomy_data['delta_neighbourhood']
+
+        taxonomy_data_dict['mobility'][data_label], _ = nan_to_num(stats.pearsonr(
+            individual, delta_individual))
+        taxonomy_data_dict['assortativity'][data_label], _ = nan_to_num(stats.pearsonr(
+            individual, neighbourhood))
+        taxonomy_data_dict['philanthropy'][data_label], _ = nan_to_num(stats.pearsonr(
+            individual, delta_neighbourhood))
+        taxonomy_data_dict['community'][data_label], _ = nan_to_num(stats.pearsonr(
+            delta_individual, neighbourhood))
+        taxonomy_data_dict['delta_assortativity'][data_label], _ = nan_to_num(stats.pearsonr(
+            delta_individual, delta_neighbourhood))
+        taxonomy_data_dict['neighbourhood_mobility'][data_label], _ = nan_to_num(stats.pearsonr(
+            neighbourhood, delta_neighbourhood))
+
+        if add_net_stats:
+            taxonomy_data_dict['equality'][data_label] = (list(
+                stats_data['gini_coeff'])[-1] + 1) / 2
+        # taxonomy_data_dict['nodes'][data_label] = list(stats_data['nodes'])[-1]
+        # taxonomy_data_dict['edges'][data_label] = list(stats_data['edges'])[-1]
+
+    return taxonomy_data_dict
+
+# ---------------------------------------------------
+
+
+def taxonomy_correlation_R2(taxonomy_data_dict):
+    grid_correlations = []
+    grid_r_square = []
+    for x_label, x_data in taxonomy_data_dict.items():
+        grid_c_line = []
+        grid_r_line = []
+        for y_label, y_data in taxonomy_data_dict.items():
+            if y_label == x_label:
+                grid_c_line.append(0)
+                grid_r_line.append(0)
+            else:
+                correlation, _ = stats.pearsonr(
+                    list(x_data.values()), list(y_data.values()))
+                grid_c_line.append(correlation)
+
+                grid_r_line.append(
+                    r2_score(list(x_data.values()), list(y_data.values())))
+
+        grid_correlations.append(grid_c_line)
+        grid_r_square.append(grid_r_line)
+
+    return grid_correlations, grid_r_square
+
+
+def PCA(X, num_components, cov_mat=None):
+    X_meaned = X - np.mean(X, axis=0)
+    if not cov_mat:
+        cov_mat = np.cov(X_meaned, rowvar=False)
+
+    eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
+
+    sorted_index = np.argsort(eigen_values)[::-1]
+    sorted_eigenvectors = eigen_vectors[:, sorted_index]
+    eigenvector_subset = sorted_eigenvectors[:, 0:num_components]
+
+    X_reduced = np.dot(eigenvector_subset.transpose(),
+                       X_meaned.transpose()).transpose()
+
+    return X_reduced
+
+
+def clustering(X, n_clusters):
+    hc = AgglomerativeClustering(
+        n_clusters=n_clusters, affinity='euclidean', linkage='ward')
+
+    y_hc = hc.fit_predict(X)
+
+    return y_hc
+
+
+# -------------------------------
--- taxonomy_plotting.py
+++ taxonomy_plotting.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm, colors, lines, patches
-from numpy.lib.type_check import nan_to_num
 
-from sklearn.metrics import r2_score
-from sklearn.cluster import AgglomerativeClustering
 
 import scipy.stats as stats
 import numpy as np
 import datetime
 
+import taxonomy_analysis
 import utilities
 
 
@@ -37,106 +35,6 @@ def custom_legend_elements(label, legend_labels, legend_elements, colour=None, m
 
 # -----------------------
 
-
-def build_taxonomy_data_dict(plot_data_dict, add_net_stats=True):
-    taxonomy_data_dict = {'mobility': {}, 'assortativity': {}, 'philanthropy': {},
-                          'community': {}, 'delta_assortativity': {}, 'neighbourhood_mobility': {}}
-    if add_net_stats:
-        taxonomy_data_dict['equality'] = {}
-        # , 'nodes': {}, 'edges': {}}
-
-    for data_f_name, plot_data in plot_data_dict.items():
-
-        taxonomy_data = plot_data['taxonomy_data']
-        if add_net_stats:
-            stats_data = plot_data['stats_data']
-        t = plot_data['t']
-        dt = plot_data['dt']
-        data_type = plot_data['data_type']
-        struc_type = plot_data['struc_type']
-
-        data_label = f"{data_f_name}: t={t} dt={dt} #{data_type}${struc_type}"
-
-        individual = taxonomy_data['individual']
-        delta_individual = taxonomy_data['delta_individual']
-        neighbourhood = taxonomy_data['neighbourhood']
-        delta_neighbourhood = taxonomy_data['delta_neighbourhood']
-
-        taxonomy_data_dict['mobility'][data_label], _ = nan_to_num(stats.pearsonr(
-            individual, delta_individual))
-        taxonomy_data_dict['assortativity'][data_label], _ = nan_to_num(stats.pearsonr(
-            individual, neighbourhood))
-        taxonomy_data_dict['philanthropy'][data_label], _ = nan_to_num(stats.pearsonr(
-            individual, delta_neighbourhood))
-        taxonomy_data_dict['community'][data_label], _ = nan_to_num(stats.pearsonr(
-            delta_individual, neighbourhood))
-        taxonomy_data_dict['delta_assortativity'][data_label], _ = nan_to_num(stats.pearsonr(
-            delta_individual, delta_neighbourhood))
-        taxonomy_data_dict['neighbourhood_mobility'][data_label], _ = nan_to_num(stats.pearsonr(
-            neighbourhood, delta_neighbourhood))
-
-        if add_net_stats:
-            taxonomy_data_dict['equality'][data_label] = (list(
-                stats_data['gini_coeff'])[-1] + 1) / 2
-        # taxonomy_data_dict['nodes'][data_label] = list(stats_data['nodes'])[-1]
-        # taxonomy_data_dict['edges'][data_label] = list(stats_data['edges'])[-1]
-
-    return taxonomy_data_dict
-
-# ---------------------------------------------------
-
-
-def taxonomy_correlation_R2(taxonomy_data_dict):
-    grid_correlations = []
-    grid_r_square = []
-    for x_label, x_data in taxonomy_data_dict.items():
-        grid_c_line = []
-        grid_r_line = []
-        for y_label, y_data in taxonomy_data_dict.items():
-            if y_label == x_label:
-                grid_c_line.append(0)
-                grid_r_line.append(0)
-            else:
-                correlation, _ = stats.pearsonr(
-                    list(x_data.values()), list(y_data.values()))
-                grid_c_line.append(correlation)
-
-                grid_r_line.append(
-                    r2_score(list(x_data.values()), list(y_data.values())))
-
-        grid_correlations.append(grid_c_line)
-        grid_r_square.append(grid_r_line)
-
-    return grid_correlations, grid_r_square
-
-
-def PCA(X, num_components, cov_mat=None):
-    X_meaned = X - np.mean(X, axis=0)
-    if not cov_mat:
-        cov_mat = np.cov(X_meaned, rowvar=False)
-
-    eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
-
-    sorted_index = np.argsort(eigen_values)[::-1]
-    sorted_eigenvectors = eigen_vectors[:, sorted_index]
-    eigenvector_subset = sorted_eigenvectors[:, 0:num_components]
-
-    X_reduced = np.dot(eigenvector_subset.transpose(),
-                       X_meaned.transpose()).transpose()
-
-    return X_reduced
-
-
-def clustering(X, n_clusters):
-    hc = AgglomerativeClustering(
-        n_clusters=n_clusters, affinity='euclidean', linkage='ward')
-
-    y_hc = hc.fit_predict(X)
-
-    return y_hc
-
-
-# -------------------------------
 
 def plot_mobility(ax, taxonomy_data, color='black', curve_label=''):
     individual = taxonomy_data['individual']
@@ -369,7 +267,8 @@ def plot_taxonomy_for_each_network(plot_data_dict, dt_percent):
 
 
 def plot_taxonomy_pairs_for_multiple_networks(plot_data_dict, dt_percent):
-    taxonomy_data_dict = build_taxonomy_data_dict(plot_data_dict, False)
+    taxonomy_data_dict = taxonomy_analysis.build_taxonomy_data_dict(
+        plot_data_dict, False)
 
     data_type_list = list(dict.fromkeys([pdd['data_type']
                                          for pdd in list(plot_data_dict.values())]))
@@ -407,11 +306,12 @@ def plot_taxonomy_pairs_for_multiple_networks(plot_data_dict, dt_percent):
 
 
 def plot_grid_taxonomy_correlations(plot_data_dict, dt_percent):
-    taxonomy_data_dict = build_taxonomy_data_dict(plot_data_dict, False)
+    taxonomy_data_dict = taxonomy_analysis.build_taxonomy_data_dict(
+        plot_data_dict, False)
     plot_labels = [label.replace('_', ' ').title().replace('Delta', 'Change in')
                    for label, _ in taxonomy_data_dict.items()]
 
-    grid_correlations, grid_r_square = taxonomy_correlation_R2(
+    grid_correlations, grid_r_square = taxonomy_analysis.taxonomy_correlation_R2(
         taxonomy_data_dict)
 
     for plot_name, grid_data in {'correlation': grid_correlations, 'R2': grid_r_square}.items():
@@ -448,7 +348,8 @@ def plot_grid_taxonomy_correlations(plot_data_dict, dt_percent):
 
 
 def plot_taxonomy_pca(plot_data_dict, dt_percent, ax=None, pca_type='corr', clus_name_pair=None, n_cluster=6, corr_mat=None):
-    taxonomy_data_dict = build_taxonomy_data_dict(plot_data_dict)
+    taxonomy_data_dict = taxonomy_analysis.build_taxonomy_data_dict(
+        plot_data_dict)
     clus_type_dict = {}
 
     if 'data_type' in clus_name_pair:
@@ -463,7 +364,8 @@ def plot_taxonomy_pca(plot_data_dict, dt_percent, ax=None, pca_type='corr', clus
 
     if not corr_mat:
         if pca_type == 'corr':
-            corr_mat, _ = taxonomy_correlation_R2(taxonomy_data_dict)
+            corr_mat, _ = taxonomy_analysis.taxonomy_correlation_R2(
+                taxonomy_data_dict)
         else:
             corr_mat = None
 
@@ -480,11 +382,11 @@ def plot_taxonomy_pca(plot_data_dict, dt_percent, ax=None, pca_type='corr', clus
         clus_type_dict['f_name'] = f_name_list
 
     if 'aggl' in clus_name_pair:
-        cluster_mat = clustering(
+        cluster_mat = taxonomy_analysis.clustering(
             np.array(list(taxonomy_data_per_dataset.values())), n_cluster)
         clus_type_dict['aggl'] = list(range(n_cluster))
 
-    pca_taxonomy = PCA(
+    pca_taxonomy = taxonomy_analysis.PCA(
         list(taxonomy_data_per_dataset.values()), 2, corr_mat)
 
     named_pca_taxonomy = {}
@@ -607,7 +509,7 @@ def plot_taxonomy_pca(plot_data_dict, dt_percent, ax=None, pca_type='corr', clus
 
 def cluster_plot(points, n_cluster, plot_name, x_label='x', y_label='y'):
     points = np.array(points)
-    cluster_mat = clustering(points, n_cluster)
+    cluster_mat = taxonomy_analysis.clustering(points, n_cluster)
 
     plot_colors = cm.ScalarMappable(colors.Normalize(
         0, n_cluster), 'tab20')
